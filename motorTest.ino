c4a448eb5234bd04dbff822f57b5117241b30f7f
slopate <31645781+slopate@users.noreply.github.com>
2019-10-27 23:19:56 -0400

    Create motorTest.ino

---
+++ motorTest.ino
@@ -0,0 +1,26 @@
+#include <Stepper.h>
+#include <ESP32Servo.h>
+
+const int stepsPerRev = 2038;
+
+Stepper myStepper(stepsPerRev, 32, 33, 25, 26);
+Servo myServo;
+int servoPos = 0;
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  myStepper.setSpeed(11);
+  myServo.attach(18);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  myStepper.step(stepsPerRev);
+  myServo.write(servoPos);
+  servoPos = servoPos + 10;
+  if (servoPos == 180) {
+      servoPos = 0;
+  }
+}
